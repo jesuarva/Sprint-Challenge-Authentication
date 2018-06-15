d03d7167d96ec60d72c33dd817a7f479e7902ef0
jesuarva <jean.ariza@jesuarva.com>
2018-06-15 19:37:45 +0200

    CLIENT: Implement 'Login'

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class Login extends Component {
-  render() {
-    return <div>Login</div>;
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
   }
+  render() {
+    return (
+      <div>
+        Login
+        <form onSubmit={this.handleSubmit} onChange={this.handleInput}>
+          <input placeholder="username" type="text" id="name" name="username" value={this.state.username} />
+          <br />
+          <input placeholder="password" type="password" id="password" name="password" value={this.state.password} />
+          <br />
+          <button type="submit">Sigin</button>
+          <br />
+          <br />
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </form>
+      </div>
+    );
+  }
+
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/users', this.state)
+      .then(response => {
+        console.log('response', response);
+        const token = response.data.token;
+        localStorage.setItem('ay! la leche!', token);
+        this.props.history.push('/jokes');
+      })
+      .catch(e => {
+        console.log('error', e);
+      });
+  };
 }
 
 export default Login;
