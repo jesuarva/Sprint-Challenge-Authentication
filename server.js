54d516652cb6f04c7dc6436ceca30518928a17f0
jesuarva <jean.ariza@jesuarva.com>
2018-06-15 18:54:19 +0200

    ADD Client starting structure

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const corsOptions = {
   // If you're moving onto the stretch problem you'll need to set this obj with the appropriate fields
   // ensure that your client's URL/Port can achieve a Handshake
   // then pass this object to the cors() function
+  origin: 'http://localhost:3000',
+  methods: [], // authorized HTTP verbs -> if ommit allow all HTTP verbs
+  credentials: true,
 };
 
 server.use(bodyParser.json());
@@ -17,5 +20,5 @@ server.use(cors());
 routes(server);
 
 module.exports = {
-  server
+  server,
 };
