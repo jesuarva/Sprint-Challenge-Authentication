9d6bdd8bb5d641b95d6d53090ed9f1902b34f13b
jesuarva <jean.ariza@jesuarva.com>
2018-06-15 19:30:22 +0200

    CLIENT: Implement Register functionality

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 class Register extends Component {
@@ -10,11 +11,41 @@ class Register extends Component {
     };
   }
   render() {
-    return <div>Register</div>;
+    return (
+      <div>
+        Register
+        <form onSubmit={this.handleSubmit} onChange={this.handleInput}>
+          <input placeholder="username" type="text" id="name" name="username" value={this.state.username} />
+          <br />
+          <input placeholder="password" type="password" id="password" name="password" value={this.state.password} />
+          <br />
+          <button type="submit">Sigin</button>
+          <br />
+          <br />
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </form>
+      </div>
+    );
   }
 
-  handleInput = () => {};
-  handlerSubmit = () => {};
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/users', this.state)
+      .then(response => {
+        console.log('token', response.data.token);
+        localStorage.setItem('ay! la leche!', response.data.token);
+        this.props.history.push('/jokes');
+      })
+      .catch(e => {
+        console.log('error', e);
+      });
+  };
 }
 
 export default Register;
