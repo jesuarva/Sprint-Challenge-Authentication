22ca4a23bb35ce82e4eab1a3058ed2e72fa93d5e
jesuarva <jean.ariza@jesuarva.com>
2018-06-16 20:08:59 +0200

    SERVER API: Refactor to includ authO

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,8 +2,27 @@ const { authenticate } = require('../utils/middlewares');
 
 const { getAllJokes, createUser, login } = require('../controllers');
 
+/**
+ * authO stuff: import needed dependencies and write a 'authCheck' function
+ */
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+const authCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    // YOUR-AUTH0-DOMAIN name e.g prosper.auth0.com
+    jwksUri: 'https://{YOUR-AUTH0-DOMAIN}/.well-known/jwks.json',
+  }),
+  // This is the identifier we set when we created the API
+  audience: 'https://jesuarva-test.eu.auth0.com/api/v2/',
+  issuer: 'jesuarva-test.eu.auth0.com',
+  algorithms: ['RS256'],
+});
+
 module.exports = server => {
-  server.get('/api/jokes', authenticate, getAllJokes);
+  server.get('/api/jokes', authCheck, getAllJokes);
   server.route('/api/users').post(createUser);
   server.route('/api/login').post(login);
 };
